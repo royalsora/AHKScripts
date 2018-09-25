2e0b492787a97b37cba15174b13a0c4f8fc9a07d
JanJulius <lang1600@student.nhl.nl>
2018-09-25 22:41:41 +0200

    Added ahk directory type

---
+++ AhkHub/AhkHub/AhkDirectory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhkHub
+{
+    class AhkDirectory
+    {
+        private string Name { get; set; }
+        private string Description { get; set; }
+        private DirectoryInfo Directory { get; set; }
+
+    }
+}
